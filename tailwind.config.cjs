8a7cfa76f9c8c1b5b0cb2fc288344515b4b40b32
jonas walden <smodcartoon@gmail.com>
2023-05-02 20:04:42 -0400

    added bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,10 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundImage: {
+        forest: "url('/forest.svg')",
+        footerStump: "url('/stumpfloor.png')",
+      },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
