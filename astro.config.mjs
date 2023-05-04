1a6f7fb49e2485b197481a7c8b1327b2479765d1
jonas walden <smodcartoon@gmail.com>
2023-05-03 21:12:54 -0400

    added party town

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://PineCode.site",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap()]
+  }), mdx(), sitemap(), partytown()]
 });
