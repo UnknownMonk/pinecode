7aac5d32368dd8280932e59ec96adf4cc6453c2e
jonas walden <smodcartoon@gmail.com>
2023-05-06 07:27:37 -0400

    added nav animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,37 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      keyframes: {
+        wobble: {
+          from: {
+            transform: "translateY(-300%)",
+          },
+
+          "15%": {
+            transform: "translateY(-250%) ",
+          },
+
+          "30%": {
+            transform: "translateY(-200%) ",
+          },
+
+          "45%": {
+            transform: "translateY(-150%, 0, 0) ",
+          },
+
+          "60%": {
+            transform: "translateY(-100%) ",
+          },
+
+          "75%": {
+            transform: "translate3d(-50%) ",
+          },
+
+          to: {
+            transform: "translateY(10%)",
+          },
+        },
+      },
       backgroundImage: {
         forest: "url('/forest.svg')",
         footerStump: "url('/stumpfloor.png')",
@@ -11,6 +42,10 @@ module.exports = {
       },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+
+      animation: {
+        wobble: "wobble 1.1s ease-out",
       },
     },
   },
